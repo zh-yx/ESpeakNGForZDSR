b6b365f9cb63016254682db94539f529e4cf6044
Zhang Yunxi <zhyx-work@outlook.com>
2026-06-21 12:29:37 +0800

    Add FindOnPath method in Utility class.

---
+++ ESpeakNG/Utility.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace ESpeakNG;
+
+internal static class Utility
+{
+    [DllImport("shlwapi.dll", CharSet = CharSet.Unicode)]
+    [return: MarshalAs(UnmanagedType.Bool)]
+    private static extern bool PathFindOnPath([In, Out] char[] pszFile, string[]? ppszOtherDirs);
+
+    public static string? FindOnPath(string fileName, string[]? additionalDirectories = null)
+    {
+        const int MAX_PATH = 260;
+        if (fileName.Length > MAX_PATH) throw new PathTooLongException();
+        char[] buffer = new char[MAX_PATH];
+        fileName.CopyTo(0, buffer, 0, fileName.Length);
+        return PathFindOnPath(buffer, additionalDirectories) ? new string(buffer, 0, Array.IndexOf(buffer, '\0')) : null;
+    }
+}
