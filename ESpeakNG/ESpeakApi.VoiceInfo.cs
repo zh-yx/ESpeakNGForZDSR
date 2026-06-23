ba90c8270b6bb574177cf85cddb4cce0ee0e823b
Zhang Yunxi <zhyx-work@outlook.com>
2026-06-23 19:14:25 +0800

    Add VoiceInfo inner class for ESpeakApi class.

---
+++ ESpeakNG/ESpeakApi.VoiceInfo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace ESpeakNG;
+
+internal partial class ESpeakApi
+{
+    public sealed class VoiceInfo
+    {
+        public required string Identifier { get; set; }
+        public required string Name { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("{0} ({1})", Name, Identifier);
+        }
+
+        public static VoiceInfo? ParseFromPtr(IntPtr voicePtr)
+        {
+            if (voicePtr == IntPtr.Zero) return null;
+            EspeakVoice voice = Marshal.PtrToStructure<EspeakVoice>(voicePtr);
+            string? identifier = Marshal.PtrToStringUTF8(voice.IdentifierPtr);
+            if (identifier == null) return null;
+            string name = Marshal.PtrToStringUTF8(voice.NamePtr) ?? string.Empty;
+            VoiceInfo info = new VoiceInfo() { Identifier = identifier, Name = name };
+            info.Identifier = identifier;
+            info.Name = name;
+            return info;
+        }
+    }
+}
